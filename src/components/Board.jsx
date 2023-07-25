fcc24b19a9c619c0e9b523d97f3c7bc9ade6d109
archita-b <archita.bhattacharyya91@gmail.com>
2023-07-25 12:17:07 +0530

    arrow movements

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,40 +2,138 @@ import { useEffect, useState } from "react";
 import styles from "./Board.module.css";
 
 export function Board() {
-  const [array, setArray] = useState(new Array(16).fill(null));
-  // console.log("arr=", array);
+  const [board, setBoard] = useState([
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+  ]);
 
   useEffect(() => {
     init();
+    document.addEventListener("keydown", handleKeyDown);
   }, []);
 
   function init() {
-    let newArray = [...array];
-    // console.log(newArray);
-    addNumber(newArray);
-    addNumber(newArray);
-    setArray(newArray);
+    const newBoard = [...board];
+    addNumber(newBoard);
+    addNumber(newBoard);
+    setBoard(newBoard);
   }
 
   function addNumber(newArray) {
-    // console.log("hi");
-    // let added = false;
-    // let arrayFull = false;
-    // while (!added) {
-    //   if (arrayFull) break;
-    // }
-    let random = Math.floor(Math.random() * 16);
-    // console.log("rand=", random);
-    if (newArray[random] === null) {
-      newArray[random] = 2;
-      // added = true;
+    let populateSquare = false;
+    let populateBoard = false;
+
+    while (!populateSquare) {
+      if (populateBoard) break;
+
+      let random1 = Math.floor(Math.random() * 4);
+      let random2 = Math.floor(Math.random() * 4);
+
+      if (newArray[random1][random2] === null) {
+        newArray[random1][random2] = Math.random() < 0.9 ? 2 : 4;
+        populateSquare = true;
+      }
+    }
+  }
+
+  function handleKeyDown(event) {
+    event.preventDefault();
+    const newBoard = [...board];
+    let moved = false;
+
+    switch (event.key) {
+      case "ArrowDown":
+        handleArrowDown();
+        break;
+      case "ArrowUp":
+        handleArrowUp();
+        break;
+      case "ArrowLeft":
+        handleArrowLeft();
+        break;
+      case "ArrowRight":
+        handleArrowRight();
+        break;
+    }
+
+    if (moved) addNumber(newBoard);
+    setBoard(newBoard);
+
+    function handleArrowDown() {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+          if (newBoard[i][j + 1] === null) {
+            newBoard[i][j + 1] = newBoard[i][j];
+            newBoard[i][j] = null;
+            moved = true;
+          } else if (newBoard[i][j + 1] === newBoard[i][j]) {
+            newBoard[i][j + 1] = newBoard[i][j] * 2;
+            moved = true;
+          }
+        }
+      }
+    }
+
+    function handleArrowUp() {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 3; j >= 0; j--) {
+          if (newBoard[i][j - 1] === null) {
+            newBoard[i][j - 1] = newBoard[i][j];
+            newBoard[i][j] = null;
+            moved = true;
+          } else if (newBoard[i][j - 1] === newBoard[i][j]) {
+            newBoard[i][j - 1] = newBoard[i][j] * 2;
+            moved = true;
+          }
+        }
+      }
+    }
+
+    function handleArrowLeft() {
+      for (let i = 3; i > 0; i--) {
+        for (let j = 0; j < 4; j++) {
+          if (newBoard[i - 1][j] === null) {
+            newBoard[i - 1][j] = newBoard[i][j];
+            newBoard[i][j] = null;
+            moved = true;
+          } else if (newBoard[i - 1][j] === newBoard[i][j]) {
+            newBoard[i - 1][j] = newBoard[i][j] * 2;
+            moved = true;
+          }
+        }
+      }
+    }
+
+    function handleArrowRight() {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 4; j++) {
+          if (newBoard[i + 1][j] === null) {
+            newBoard[i + 1][j] = newBoard[i][j];
+            newBoard[i][j] = null;
+            moved = true;
+          } else if (newBoard[i + 1][j] === newBoard[i][j]) {
+            newBoard[i + 1][j] = newBoard[i][j] * 2;
+            moved = true;
+          }
+        }
+      }
     }
   }
 
   return (
     <div className={styles.board}>
-      {array.map((element) => {
-        return <div className={styles.square}>{element}</div>;
+      {board.map((row, rowId) => {
+        return (
+          <div key={rowId} className={styles.row}>
+            {row.map((square, colId) => (
+              <div key={colId} className={styles.square}>
+                {square}
+              </div>
+            ))}
+          </div>
+        );
       })}
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export function Footer() {
   return (
     <div className={styles.howToPlay} id="how-to-play">
       <strong>HOW TO PLAY:</strong> Use your <strong>arrow keys</strong> to move
-      the tiles. Tiles with same number <strong>merge into one</strong>
-      when they touch. Add them up to reach <strong>2048!</strong>
+      the tiles. Tiles with same number <strong>merge into one</strong> when
+      they touch. Add them up to reach <strong>2048!</strong>
       <br />
       <a href="" className={styles.startPlayingLink}>
         <strong>Start playing{"\u2192"}</strong>
