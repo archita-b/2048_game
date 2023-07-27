051b4b46c3645f0426f998f29eea5c95dd1c8992
archita-b <archita.bhattacharyya91@gmail.com>
2023-07-27 19:42:02 +0530

    refactor

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,153 +1,26 @@
 import { useEffect, useState } from "react";
 import styles from "./Board.module.css";
+import {
+  getEmptyBoard,
+  handleArrowDown,
+  handleArrowLeft,
+  handleArrowRight,
+  handleArrowUp,
+  handleKeyDown,
+  init,
+} from "../game.js";
 
 export function Board() {
-  const [board, setBoard] = useState([
-    [null, null, null, null],
-    [null, null, null, null],
-    [null, null, null, null],
-    [null, null, null, null],
-  ]);
+  const [board, setBoard] = useState(() => getEmptyBoard());
 
   useEffect(() => {
-    init();
-    document.addEventListener("keydown", handleKeyDown);
+    const newBoard = [...board];
+    init(newBoard);
+    setBoard(newBoard);
+    document.addEventListener("keydown", (event) => {
+      handleKeyDown(event, board, setBoard);
+    });
   }, []);
-
-  function init() {
-    const newBoard = [...board];
-    addNumber(newBoard);
-    addNumber(newBoard);
-    setBoard(newBoard);
-  }
-
-  function addNumber(newArray) {
-    let populateSquare = false;
-
-    let random1 = Math.floor(Math.random() * 4);
-    let random2 = Math.floor(Math.random() * 4);
-
-    if (newArray[random1][random2] === null) {
-      newArray[random1][random2] = Math.random() < 0.9 ? 2 : 4;
-      populateSquare = true;
-    }
-  }
-
-  function handleKeyDown(event) {
-    event.preventDefault();
-    const newBoard = [...board];
-    let moved = false;
-
-    switch (event.key) {
-      case "ArrowDown":
-        handleArrowDown();
-        break;
-      case "ArrowUp":
-        handleArrowUp();
-        break;
-      case "ArrowLeft":
-        handleArrowLeft();
-        break;
-      case "ArrowRight":
-        handleArrowRight();
-        break;
-      default:
-        break;
-    }
-
-    // if (moved) {
-    addNumber(newBoard);
-    setBoard(newBoard);
-    // }
-
-    function handleArrowDown() {
-      for (let col = 0; col < 4; col++) {
-        for (let row = 2; row >= 0; row--) {
-          let currentRow = row;
-          while (currentRow < 3 && newBoard[col][currentRow] !== null) {
-            if (newBoard[col][currentRow + 1] === null) {
-              newBoard[col][currentRow + 1] = newBoard[col][currentRow];
-              newBoard[col][currentRow] = null;
-              currentRow++;
-              moved = true;
-            } else if (
-              newBoard[col][currentRow + 1] === newBoard[col][currentRow]
-            ) {
-              newBoard[col][currentRow + 1] *= 2;
-              newBoard[col][currentRow] = null;
-              moved = true;
-            } else break;
-          }
-        }
-      }
-    }
-
-    function handleArrowUp() {
-      for (let col = 0; col < 4; col++) {
-        for (let row = 1; row < 4; row++) {
-          let currentRow = row;
-          while (currentRow > 0 && newBoard[col][currentRow] !== null) {
-            if (newBoard[col][currentRow - 1] === null) {
-              newBoard[col][currentRow - 1] = newBoard[col][currentRow];
-              newBoard[col][currentRow] = null;
-              currentRow--;
-              moved = true;
-            } else if (
-              newBoard[col][currentRow - 1] === newBoard[col][currentRow]
-            ) {
-              newBoard[col][currentRow - 1] *= 2;
-              newBoard[col][currentRow] = null;
-              moved = true;
-            } else break;
-          }
-        }
-      }
-    }
-
-    function handleArrowLeft() {
-      for (let col = 1; col < 4; col++) {
-        for (let row = 0; row < 4; row++) {
-          let currentCol = col;
-          while (currentCol > 0 && newBoard[currentCol][row] !== null) {
-            if (newBoard[currentCol - 1][row] === null) {
-              newBoard[currentCol - 1][row] = newBoard[currentCol][row];
-              newBoard[currentCol][row] = null;
-              currentCol--;
-              moved = true;
-            } else if (
-              newBoard[currentCol - 1][row] === newBoard[currentCol][row]
-            ) {
-              newBoard[currentCol - 1][row] *= 2;
-              newBoard[currentCol][row] = null;
-              moved = true;
-            } else break;
-          }
-        }
-      }
-    }
-
-    function handleArrowRight() {
-      for (let col = 2; col >= 0; col--) {
-        for (let row = 0; row < 4; row++) {
-          let currentCol = col;
-          while (currentCol < 3 && newBoard[currentCol][row] !== null) {
-            if (newBoard[currentCol + 1][row] === null) {
-              newBoard[currentCol + 1][row] = newBoard[currentCol][row];
-              newBoard[currentCol][row] = null;
-              currentCol++;
-              moved = true;
-            } else if (
-              newBoard[currentCol + 1][row] === newBoard[currentCol][row]
-            ) {
-              newBoard[currentCol + 1][row] *= 2;
-              newBoard[currentCol][row] = null;
-              moved = true;
-            } else break;
-          }
-        }
-      }
-    }
-  }
 
   return (
     <div className={styles.board}>
---
+++ src/game.js
@@ -0,0 +1,152 @@
+export function getEmptyBoard() {
+  return [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+  ];
+}
+
+export function init(board) {
+  addNumber(board);
+  addNumber(board);
+  return board;
+}
+
+function addNumber(board) {
+  let added = false;
+  let boardFull = false;
+
+  while (!added) {
+    if (boardFull) break;
+
+    let random1 = Math.round(Math.random() * 3);
+    let random2 = Math.round(Math.random() * 3);
+
+    if (board[random1][random2] === null) {
+      board[random1][random2] = Math.random() < 0.9 ? 2 : 4;
+      added = true;
+    }
+  }
+}
+
+export function handleKeyDown(event, board, setBoard) {
+  event.preventDefault();
+
+  const newBoard = [...board];
+
+  switch (event.key) {
+    case "ArrowDown":
+      handleArrowDown(newBoard);
+      setBoard(newBoard);
+      break;
+    case "ArrowUp":
+      handleArrowUp(newBoard);
+      setBoard(newBoard);
+      break;
+    case "ArrowLeft":
+      handleArrowLeft(newBoard);
+      setBoard(newBoard);
+      break;
+    case "ArrowRight":
+      handleArrowRight(newBoard);
+      setBoard(newBoard);
+      break;
+  }
+}
+
+export function handleArrowDown(board) {
+  const oldBoard = JSON.parse(JSON.stringify(board));
+
+  for (let col = 0; col < 4; col++) {
+    for (let row = 2; row >= 0; row--) {
+      let currentRow = row;
+      while (currentRow < 3 && board[col][currentRow] !== null) {
+        if (board[col][currentRow + 1] === null) {
+          board[col][currentRow + 1] = board[col][currentRow];
+          board[col][currentRow] = null;
+          currentRow++;
+        } else if (board[col][currentRow + 1] === board[col][currentRow]) {
+          board[col][currentRow + 1] *= 2;
+          board[col][currentRow] = null;
+        } else break;
+      }
+    }
+  }
+  if (JSON.stringify(oldBoard) !== JSON.stringify(board)) {
+    addNumber(board);
+  }
+  return board;
+}
+
+export function handleArrowUp(board) {
+  const oldBoard = JSON.parse(JSON.stringify(board));
+
+  for (let col = 0; col < 4; col++) {
+    for (let row = 1; row < 4; row++) {
+      let currentRow = row;
+      while (currentRow > 0 && board[col][currentRow] !== null) {
+        if (board[col][currentRow - 1] === null) {
+          board[col][currentRow - 1] = board[col][currentRow];
+          board[col][currentRow] = null;
+          currentRow--;
+        } else if (board[col][currentRow - 1] === board[col][currentRow]) {
+          board[col][currentRow - 1] *= 2;
+          board[col][currentRow] = null;
+        } else break;
+      }
+    }
+  }
+  if (JSON.stringify(oldBoard) !== JSON.stringify(board)) {
+    addNumber(board);
+  }
+  return board;
+}
+
+export function handleArrowLeft(board) {
+  const oldBoard = JSON.parse(JSON.stringify(board));
+
+  for (let col = 1; col < 4; col++) {
+    for (let row = 0; row < 4; row++) {
+      let currentCol = col;
+      while (currentCol > 0 && board[currentCol][row] !== null) {
+        if (board[currentCol - 1][row] === null) {
+          board[currentCol - 1][row] = board[currentCol][row];
+          board[currentCol][row] = null;
+          currentCol--;
+        } else if (board[currentCol - 1][row] === board[currentCol][row]) {
+          board[currentCol - 1][row] *= 2;
+          board[currentCol][row] = null;
+        } else break;
+      }
+    }
+  }
+  if (JSON.stringify(oldBoard) !== JSON.stringify(board)) {
+    addNumber(board);
+  }
+  return board;
+}
+
+export function handleArrowRight(board) {
+  const oldBoard = JSON.parse(JSON.stringify(board));
+
+  for (let col = 2; col >= 0; col--) {
+    for (let row = 0; row < 4; row++) {
+      let currentCol = col;
+      while (currentCol < 3 && board[currentCol][row] !== null) {
+        if (board[currentCol + 1][row] === null) {
+          board[currentCol + 1][row] = board[currentCol][row];
+          board[currentCol][row] = null;
+          currentCol++;
+        } else if (board[currentCol + 1][row] === board[currentCol][row]) {
+          board[currentCol + 1][row] *= 2;
+          board[currentCol][row] = null;
+        } else break;
+      }
+    }
+  }
+  if (JSON.stringify(oldBoard) !== JSON.stringify(board)) {
+    addNumber(board);
+  }
+  return board;
+}
